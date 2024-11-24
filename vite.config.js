e65faed4d18fb05d018073acadf98bbf9c308da9
adbenitez <asieldbenitez@gmail.com>
2024-11-24 05:51:10 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { webxdcViteConfigNoLegacy } from "@webxdc/vite-plugins";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(webxdcViteConfigNoLegacy());
